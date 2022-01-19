e67b8bae38e14030cb92adc31da5d20c8dd5940b
Nyanjuimarvin <marnyanjui@gmail.com>
2022-01-19 19:43:34 +0300

    Resolve promise on clicking search button

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,23 @@
-const searchTerm = document.querySelector("#userquery");
+const searchQuery = document.querySelector("#userquery");
 const searchButton = document.querySelector("#searchbutton");
+const movieBox = document.querySelector("#moviecheck");
+const gameBox = document.querySelector("#gamecheck");
+const bookBox = document.querySelector("#bookcheck");
+
+async function getGameData() {
+  if (movieBox.checked !== false) {
+    const searchTerm = searchQuery.value;
+    const res = await axios.get(
+      `https://api.rawg.io/api/games?key=94b30e61ab464d5391dad2327cbde848&page_size=60&search=${searchTerm}`
+    );
+    console.log(res.data.results);
+  } else {
+    alert("ERROR!!!");
+  }
+}
+
+searchButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  getGameData();
+  searchQuery.value = "";
+});
