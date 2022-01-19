4572612c9cc806a347961c8a24ecae273375c6c6
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-01-19 20:24:40 +0300

    Created a function to Get Movies

--- JS/app.js
+++ JS/app.js
@@ -8,3 +8,41 @@ $(document).ready(()=>{
 		
 	})
 })
+
+function getGames(searchText){
+	const settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://free-epic-games.p.rapidapi.com/free",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "free-epic-games.p.rapidapi.com",
+			"x-rapidapi-key": "1da5882939mshf7a00a599c4e78cp163474jsnd820c1ecaed8"
+		}
+	};
+	
+	$.ajax(settings).done(function (response) {
+		console.log(response);
+
+		movies= response.data
+		let output  = ''
+		$.each(movies,(index, movie)=>{
+			output += `
+			<div class="col-md-3">
+    			<div class="well-text-center">
+        		<img src=${movie.id}>
+        		<h5>${movie.title}</h5>
+
+
+    		</div>
+		</div>
+			
+			`
+		})
+
+		$("#movies").html(output)
+		
+
+
+	});
+}
