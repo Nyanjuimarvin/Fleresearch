220482a9f1ebd0fd88aeafd5e4589910580450e5
Caleb_ <95022986+Mzazi25@users.noreply.github.com>
2022-01-20 05:38:13 +0300

    Update Js

--- JS/app.js
+++ JS/app.js
@@ -1,86 +1,88 @@
-$(document).ready(()=>{
-	$("#searchForm").submit(function(event){
-		let searchText= $("#userquery").val()
-		console.log(searchText)
-		getGames(searchText)
-		getBooks(searchText)
-	
-		event.preventDefault()
-		
-	})
-})
-
-function getGames(searchText){
-	const settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://free-epic-games.p.rapidapi.com/free",
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "free-epic-games.p.rapidapi.com",
-			"x-rapidapi-key": "1da5882939mshf7a00a599c4e78cp163474jsnd820c1ecaed8"
-		}
-	};
-	
-	$.ajax(settings).done(function (response) {
-		console.log(response);
-
-		games= response.data
-		let output  = ''
-		$.each(games,(index, games)=>{
-			output += `
-			<div class="col-md-3">
-    			<div class="well-text-center">
-        		<img src=${games.id}>
-        		<h5>${games.title}</h5>
-
-
-    		</div>
-		</div>
-			
-			`
-		})
-
-		$("#games").html(output)
-		
-
-
+$(document).ready(() => {
+	$("#searchForm").submit(function (event) {
+	  let searchText = $("#userquery").val();
+	  // console.log(searchText)
+	  getGames(searchText);
+	  getBooks(searchText);
+  
+	  event.preventDefault();
 	});
-}
-
-function getBooks(searchText){
+  });
+  
+  function getGames(searchText) {
 	const settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://book4.p.rapidapi.com/",
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "book4.p.rapidapi.com",
-			"x-rapidapi-key": "1da5882939mshf7a00a599c4e78cp163474jsnd820c1ecaed8"
-		}
+	  async: true,
+	  crossDomain: true,
+	  url: "https://free-epic-games.p.rapidapi.com/free",
+	  method: "GET",
+	  dataType: "json",
+	  headers: {
+		"x-rapidapi-host": "free-epic-games.p.rapidapi.com",
+		"x-rapidapi-key": "1da5882939mshf7a00a599c4e78cp163474jsnd820c1ecaed8",
+	  },
 	};
-	
+  
 	$.ajax(settings).done(function (response) {
-		console.log(response);
-		movies= response.data
-		let output  = ''
-		$.each(books,(index, book)=>{
-			output += `
-			<div class="col-md-3">
-    			<div class="well-text-center">
-        		<img src=${book.id}>
-        		<h5>${book.title}</h5>
-
-
-    		</div>
-		</div>
-			
-			`
-		})
-
-		$("#books").html(output)
-		
-
-
+	  // console.log(response);
+  
+	  games = response.current;
+	  let output = "";
+	  $.each(games, function (index, game) {
+		var g = game.current;
+		$.each(g, function (index, game) {
+		  console.log(game)
+		  output += `
+			  <div class="col-md-3">
+				  <div class="well-text-center">
+				  <img src=${game.url}>
+				  <h5>${game.title}</h5>
+  
+  
+			  </div>
+		  </div>
+			  
+			  `;
+		});
+	  });
+  
+	  $("#games").html(output);
 	});
-}
+  }
+  
+  function getBooks(searchText) {
+	const settings = {
+	  async: true,
+	  crossDomain: true,
+	  url: "https://book4.p.rapidapi.com/",
+	  method: "GET",
+	  dataType: "json",
+	  headers: {
+		"x-rapidapi-host": "book4.p.rapidapi.com",
+		"x-rapidapi-key": "1da5882939mshf7a00a599c4e78cp163474jsnd820c1ecaed8",
+	  },
+	};
+  
+	$.ajax(settings).done(function (response) {
+	  // console.log(response);
+	  books = response;
+	  let output = "";
+	  $.each(books, function (index, book) {
+		var b = book;
+		$.each(b, function (index, book) {
+		  output += `
+			  <div class="col-md-3">
+				  <div class="well-text-center">
+				  <img src=${book.url}>
+				  <h5>${book.title}</h5>
+  
+  
+			  </div>
+		  </div>
+			  
+			  `;
+		});
+	  });
+  
+	  $("#books").html(output);
+	});
+  }
